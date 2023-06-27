1fc542a3c476dd8151cbd41bbae25d533219a2bd
Isaac Lin <me@isaaclin.cn>
2023-06-27 19:02:02 +0800

    fix: set `proofPollingInterval` to `time.Duration`

--- prover/proof_producer/zkevm_rpcd_producer.go
+++ prover/proof_producer/zkevm_rpcd_producer.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	errProofGenerating   = errors.New("proof is generating")
-	proofPollingInterval = 1 * time.Second
+	proofPollingInterval = time.Duration(1) * time.Second
 )
 
 // ZkevmRpcdProducer is responsible for requesting zk proofs from the given proverd endpoint.
