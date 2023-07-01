7eeb9e49e57544f3187e8e9eec4d2d7146837a08
Isaac Lin <me@isaaclin.cn>
2023-07-01 13:53:00 +0800

    chore: update `gasTipCap` to 1555 gwei
    competitor tx at 1310 gwei: https://sepolia.etherscan.io/tx/0x71ef9d4fd641f630698fead150464845cfa4416e5eaa964001067690ad5b26cd

--- prover/proof_submitter/util.go
+++ prover/proof_submitter/util.go
@@ -59,9 +59,9 @@ func getProveBlocksTxOpts(
 	// 	}
 	// }
 
-	// Hard set the gasPrice to 620 gwei
+	// Hard set the gasPrice to 1555 gwei
 	// TODO: Use dynamic gas strategy (competitor: https://sepolia.etherscan.io/address/0x62009c027b47c94dcd569aba8226ce024b81d324)
-	opts.GasTipCap = big.NewInt(1001000000000)
+	opts.GasTipCap = big.NewInt(1555000000000)
 	opts.GasLimit = uint64(1500000)
 
 	return opts, nil
