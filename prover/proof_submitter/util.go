a45619714990674337ce65f65e9cf4f355c6b5f5
Isaac Lin <me@isaaclin.cn>
2023-07-18 01:59:25 +0800

    chore: update `baseGas` to 13500 gwei
    Competitor tx at 13183 gwei: https://sepolia.etherscan.io/tx/0xc5721c8cf17e1ceed12bd373ec877a7cffeae4b9672f0bdb6e262bff92defac8

--- prover/proof_submitter/util.go
+++ prover/proof_submitter/util.go
@@ -71,8 +71,8 @@ func getProveBlocksTxOpts(
 	// 	}
 	// }
 
-	// Randomly set the gasPrice to (12500 + random value) gwei
-	opts.GasTipCap = randomGas(big.NewInt(12500000000000))
+	// Randomly set the gasPrice to (13500 + random value) gwei
+	opts.GasTipCap = randomGas(big.NewInt(13500000000000))
 	opts.GasLimit = uint64(1500000)
 
 	return opts, nil
