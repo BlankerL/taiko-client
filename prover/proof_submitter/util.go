6da74a7413173c47f2b9373c255d026d65541713
Isaac Lin <me@isaaclin.cn>
2023-07-20 11:37:01 +0800

    chore: udpate `baseGas` to 2000 gwei
    Competitor tx at 943 gwei: https://sepolia.etherscan.io/tx/0x089f13983dd955503f4fdd05e2be207a16666a40f4f707dac54ac52884f4e743

--- prover/proof_submitter/util.go
+++ prover/proof_submitter/util.go
@@ -71,8 +71,8 @@ func getProveBlocksTxOpts(
 	// 	}
 	// }
 
-	// Randomly set the gasPrice to (800 + random value) gwei
-	opts.GasTipCap = randomGas(big.NewInt(800000000000))
+	// Randomly set the gasPrice to (2000 + random value) gwei
+	opts.GasTipCap = randomGas(big.NewInt(2000000000000))
 	opts.GasLimit = uint64(1500000)
 
 	return opts, nil
